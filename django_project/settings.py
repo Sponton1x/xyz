c7b2fb4b883aeedd8403256c1688fe51ba76c1d6
Connective <76740905+Sponton1x@users.noreply.github.com>
2024-06-28 17:56:25 +0200

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -146,7 +146,7 @@ STATICFILES_DIRS = [
 ]
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
